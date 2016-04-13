2343071dadb84700932be41f34b54429104c0dfe
mike <mike.fch1@gmail.com>
2016-04-14 00:33:24 +0300

    init

---
+++ project/LogsServiceBuild.scala
@@ -0,0 +1,42 @@
+import sbt._
+import sbt.Keys._
+
+object LogsServiceBuild extends Build {
+
+  val baseSettings = Seq(
+    scalacOptions ++= Seq(
+      "-deprecation",
+      "-encoding", "UTF-8",
+      "-feature",
+      "-language:existentials",
+      "-language:higherKinds",
+      "-language:implicitConversions",
+      "-unchecked",
+      "-Yno-adapted-args",
+      "-Ywarn-dead-code",
+      "-Ywarn-numeric-widen",
+      "-Xfuture",
+      "-Xlint"
+    ),
+    resolvers += Resolver.sonatypeRepo("snapshots"),
+    resolvers += Resolver.sonatypeRepo("releases"),
+    scalaVersion := "2.11.6"
+  ) 
+  
+  
+  lazy val main = Project(
+    id = "logs-service",
+    base = file("."),
+    settings = baseSettings ++ Seq(
+      organization := "com.github.scalalab3-logs",
+      name := "logs-service",
+      version := "0.0.1",
+      licenses += ("MIT", url("http://opensource.org/licenses/MIT")),
+      libraryDependencies ++= Seq()
+    )
+  ) 
+
+  
+
+
+}
---
+++ src/main/scala/Boot.scala
@@ -0,0 +1,3 @@
+object Boot extends App {
+  println(1)
+}
