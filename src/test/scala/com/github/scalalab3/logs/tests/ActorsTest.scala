b5be229f1b7d45d9f5dc4af462adf729c87aa3e9
Kirill Pinchuk <cybergrind@gmail.com>
2016-05-19 22:04:44 +0300

    Test: correct akka lifecycle and fail on DeadLetter (#25)
    * Test: correct akka lifecycle and fail on DeadLetter
    
    * Akka: remove extra file from commit

--- src/test/scala/com/github/scalalab3/logs/tests/ActorsTest.scala
+++ src/test/scala/com/github/scalalab3/logs/tests/ActorsTest.scala
@@ -1,9 +1,10 @@
 package com.github.scalalab3.logs.services
 
-import akka.actor.{Props}
+import akka.actor.Props
 import com.github.scalalab3.logs.tests.AkkaSpec
 
 class SystemActorTest extends AkkaSpec {
+
   "Test SystemActor" >> {
     val actorRef = system.actorOf(Props(classOf[SystemActor]), "system-actor")
 
