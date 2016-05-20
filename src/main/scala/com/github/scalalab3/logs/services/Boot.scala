837bcd0ea0f44147cd7852efbfa377208ba924a8
Anton Larionov <anton2larionov@gmail.com>
2016-05-21 00:40:14 +0300

    Delete unnecessary files

--- src/main/scala/com/github/scalalab3/logs/services/Boot.scala
+++ src/main/scala/com/github/scalalab3/logs/services/Boot.scala
@@ -1,19 +1,9 @@
 package com.github.scalalab3.logs.services
 
 import akka.actor.{ActorSystem, Props}
-import akka.io.IO
-import akka.util.Timeout
-import com.github.scalalab3.logs.config.SprayConfig
-import spray.can.Http
-import scala.concurrent.duration._
 
 object Boot extends App {
-  val config = SprayConfig.load()
-
-  implicit val timeout = Timeout(5.seconds)
   implicit val system = ActorSystem("logs-service")
-
-  val systemActor = system.actorOf(Props[SystemActor], "system-actor")
-
-  IO(Http) ! Http.Bind(systemActor, interface = config.host, port = config.port)
+  system.actorOf(Props(classOf[SystemActor]), "system-actor")
+  println("Call boot")
 }
--- src/main/scala/com/github/scalalab3/logs/services/SystemActor.scala
+++ src/main/scala/com/github/scalalab3/logs/services/SystemActor.scala
@@ -1,20 +1,9 @@
 package com.github.scalalab3.logs.services
 
-import akka.actor._
-import com.github.scalalab3.logs.services.query.QueryServiceRoute
-import spray.routing._
+import akka.actor.{Actor, ActorLogging}
 
-import scala.concurrent.ExecutionContext
-
-// QueryServiceRoute will be replaced with smth like:
-// trait AppRoutes with QueryServiceRoute with AnalyticsServiceRoute with ReadRoute ... {
-//   runRoute(queryRoute ~ analyticsRoute ~ readRoute ~ ...)
-// }
-class SystemActor extends HttpServiceActor with ActorLogging
-   with QueryServiceRoute with ActorContextCreationSupport with StorageProvider {
-
-  override def exc: ExecutionContext = context.dispatcher
-  override val dbService = context.actorOf(Props(classOf[DummyStorageActor]), "db-actor")
-
-  def receive = runRoute(queryRoute)
+class SystemActor extends Actor with ActorLogging {
+  def receive = {
+    case msg => log.warning(s"Unhandled message: $msg")
+  }
 }
