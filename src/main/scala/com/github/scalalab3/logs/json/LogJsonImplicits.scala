330d72e94b587770c46ea2c26c005418e9d8c846
Anton Larionov <anton2larionov@gmail.com>
2016-05-21 02:13:02 +0300

    Cleanup json implicits

--- src/main/scala/com/github/scalalab3/logs/json/LogJsonImplicits.scala
+++ src/main/scala/com/github/scalalab3/logs/json/LogJsonImplicits.scala
@@ -5,14 +5,12 @@ import play.api.libs.json.{JsString, JsValue, Json, Writes}
 import spray.http.ContentTypes.`application/json`
 import spray.httpx.marshalling.Marshaller
 
-object LogJsonProtocol {
-
+object LogJsonImplicits {
   implicit val levelJsonFormat = new Writes[Level] {
     override def writes(x: Level): JsValue = JsString(x.toString)
   }
 
   implicit val logFormat = Json.writes[Log]
 
-  implicit val marshaller = Marshaller
-    .delegate[Seq[Log], String](`application/json`)(_.map(Json.toJson(_)).mkString("[", ",", "]"))
+  implicit val marshaller = Marshaller.delegate[Seq[Log], String](`application/json`)(Json.toJson(_).toString())
 }
--- src/main/scala/com/github/scalalab3/logs/services/query/QueryServiceRoute.scala
+++ src/main/scala/com/github/scalalab3/logs/services/query/QueryServiceRoute.scala
@@ -3,7 +3,7 @@ package com.github.scalalab3.logs.services.query
 import akka.actor.Props
 import akka.pattern.ask
 import akka.util.Timeout
-import com.github.scalalab3.logs.json.LogJsonProtocol._
+import com.github.scalalab3.logs.json.LogJsonImplicits._
 import com.github.scalalab3.logs.services.messages._
 import com.github.scalalab3.logs.services.{ActorCreationSupport, StorageProvider}
 import spray.http.StatusCodes
@@ -13,8 +13,8 @@ import spray.routing.HttpService
 import scala.concurrent.ExecutionContext
 import scala.concurrent.duration._
 
-trait QueryServiceRoute extends HttpService {
-  self: ActorCreationSupport with StorageProvider =>
+trait QueryServiceRoute extends HttpService with ActorCreationSupport {
+  self: StorageProvider =>
 
   implicit val timeout = Timeout(5.seconds)
   implicit def exc: ExecutionContext
